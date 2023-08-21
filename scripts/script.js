a20b19fd64d88d784e187031bbc89886fee5f2f3
Sushmita Adannavar <sushmitaadannavar90@gmail.com>
2023-08-21 08:33:31 +0530

    Added employee payroll

---
+++ scripts/script.js
@@ -0,0 +1,143 @@
+//$(document).ready(function () {
+
+//$("#myBtn").click(function () {
+function addEmployee() {
+    event.defaultPrevented
+
+    var Name = $("#name").val();
+    console.log(Name);
+
+    var profileImage = $("input[name='Profileimage']:checked").val();
+    console.log(profileImage);
+
+    var gender = $("input[name='Gender']:checked").val();
+    console.log(gender);
+
+    var arr = [];
+    $.each($("input[name='Department']:checked"), function () {
+        arr.push($(this).val());
+    });
+    console.log(arr.join(","));
+    arr = arr.join(" ")
+
+    var Salary = $("#salary").val();
+    console.log(Salary);
+
+    var Date = $("#date").val();
+    var Month = $("#month").val();
+    var Year = $("#year").val();
+    console.log(`${Date}/${Month}/${Year}`);
+    let StartDate = `${Date} ${Month} ${Year}`;
+
+    var Notes = $("#notes").val();
+    console.log(Notes);
+
+    let requestData = {
+
+        "Name": Name,
+        "profileImage": profileImage,
+        "Gender": gender,
+        "Department": arr,
+        "Salary": Salary,
+        "StartDate": StartDate,
+        "Notes": Notes
+    }
+
+    $.ajax({
+        type: "POST",
+        url: 'http://localhost:3000/employee',
+        data: JSON.stringify(requestData),
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function (response) {
+
+            console.log("Data stored", response)
+        }
+    });
+}
+// })
+//})
+if (location.hash === '#execute') {
+    let urlParams = new URLSearchParams(location.search);
+    let empId = urlParams.get('Id');
+    $.ajax({
+        url: 'http://localhost:3000/employee/' + empId,
+        contentType: "application/json; charset=utf-8",
+        type: 'GET',
+        dataType: 'json',
+        success: function (result, textStatus, xhr) {
+            $("#name").val(result.Name);
+            $("input[name='Profileimage']:checked").val(result.profileImage);
+            $("input[name='Gender']:checked").val(result.Gender);
+            $.each($("input[name='Department']:checked"), function () {
+                arr.push($(this).val(result.Department));
+            });
+            $("#salary").val(result.Salary);
+            let datestring = result.StartDate.split(" ")
+            $("#date").val(datestring[0]);
+            $("#month").val(datestring[1]);
+            $("#year").val(datestring[2]);
+            $("#notes").val(result.Notes);
+
+        },
+        error: function (xhr, textStatus, errorthrown) {
+            alert("Error in operation");
+        }
+    })
+    function updateEmployee(){
+        var Name = $("#name").val();
+    console.log(Name);
+
+    var profileImage = $("input[name='Profileimage']:checked").val();
+    console.log(profileImage);
+
+    var gender = $("input[name='Gender']:checked").val();
+    console.log(gender);
+
+    var arr = [];
+    $.each($("input[name='Department']:checked"), function () {
+        arr.push($(this).val());
+    });
+    console.log(arr.join(","));
+    arr = arr.join(" ")
+
+    var Salary = $("#salary").val();
+    console.log(Salary);
+
+    var Date = $("#date").val();
+    var Month = $("#month").val();
+    var Year = $("#year").val();
+    console.log(`${Date}/${Month}/${Year}`);
+    let StartDate = `${Date} ${Month} ${Year}`;
+
+    var Notes = $("#notes").val();
+    console.log(Notes);
+
+    let requestData = {
+
+        "Name": Name,
+        "profileImage": profileImage,
+        "Gender": gender,
+        "Department": arr,
+        "Salary": Salary,
+        "StartDate": StartDate,
+        "Notes": Notes
+    }
+    alert(empId)
+if(empId){
+    $.ajax({
+        type: "PUT",
+        url: 'http://localhost:3000/employee/'+empId,
+        data: JSON.stringify(requestData),
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function (response) {
+             
+            console.log("Data updated", response)
+        }
+    });
+    }
+    }
+}
+
+
---
+++ scripts/scriptdetails.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function () {
+   $.ajax({
+    url:'http://localhost:3000/employee',
+    method:'GET',
+    contentType: "application/json; charset=utf-8",
+    dataType:'json',
+    
+    success:function(result,textStatus,xhr){
+        var trHTML='';
+
+        $.each(result,function(i,item){
+
+            trHTML+=`<tr class="tableentrytype"><td class="nameform">`+`<img src='`+item.profileImage+`' height=20 width="20" class="img1">`+item.Name+`</td><td class="genderform">`+item.Gender+`</td><td class="departmentform">`+item.Department+`</td><td class="salaryform">`+item.Salary+`</td><td class="startDateform">`+item.StartDate+`</td><td><button  type="button" onclick="deleteEmployee(${(item.id)})" class="deletingButton"><img src="../Assets/Screenshot 2023-08-18 103047.jpg" alt="logo" width=19px
+            height=19px ></button>
+        <button  type="button" onclick="editEmployee(${(item.id)})" class="editingbutton"><img src="../Assets/Screenshot 2023-08-18 103015.jpg" alt="logo" width=18px height=18px></button></td></tr>`;
+            
+        });
+        console.log(trHTML);
+       $('#Empdetails').append(trHTML);
+    },
+    error:function(xhr,textStatus,errorthrown){
+        console.log("Error in operation");
+    }
+})
+})
+
+function deleteEmployee(empId){
+    $.ajax({
+    url:'http://localhost:3000/employee/'+empId,
+    type:'DELETE',
+    dataType:'json',
+    success:function(result,textStatus,xhr){
+        alert("Deleted data");
+    },
+    error:function(xhr,textStatus,errorthrown){
+        console.log("Error in operation");
+    }
+    })
+}
+function editEmployee(empId){
+    url='Empform.html'
+     location.href=`${url}?Id=${empId}#execute`;
+ }
+
